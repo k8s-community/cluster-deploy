59df02021a9b982cc828f8997bd77532c220c1d5
Vladislav Saveliev <vsaveliev@openprovider.nl>
2017-09-07 18:44:17 +0700

    added compiled helm charts from kubernetes stable charts

---
+++ roles/prometheus/templates/deploy-prometheus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO: add deploy function(-s)
+
+#function deploy_prometheus {
+#    if kubectl get deploy -l k8s-app=prometheus | grep prometheus &> /dev/null; then
+#        echo "Prometheus already exists"
+#    else
+#        echo "Creating Prometheus"
+#        kubectl create -f {{ k8s_addons_dir }}/prometheus.yaml
+#    fi
+#
+#    echo
+#}
+#
+#deploy_prometheus
