f7bff2b115d5304a5abfba50cc3a9b172fccff67
Elena Grahovac <elena@grahovac.me>
2017-09-18 15:44:29 +0700

    Drop users if they already exists to be able to run this playbook multiple times

--- roles/cicd/templates/deploy-k8s-community-user-db.sh
+++ roles/cicd/templates/deploy-k8s-community-user-db.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+# Drop users if they already exist
+function user_rm() {
+  kubectl exec "cockroachdb-${1}" -- /cockroach/cockroach user rm ${2} \
+      --host "cockroachdb-${1}.cockroachdb" \
+      --insecure
+}
+
+user_rm 0 {{ k8s_community_db_username }}
+user_rm 0 {{ k8s_github_integration_db_username }}
+
+# Create database and user with priveleges.
 function sql() {
   kubectl exec "cockroachdb-${1}" -- /cockroach/cockroach sql \
       --host "cockroachdb-${1}.cockroachdb" \
@@ -9,7 +20,6 @@ function sql() {
       -e "$(cat /dev/stdin)"
 }
 
-# Create database and user with priveleges.
 cat <<EOF | sql 0
 CREATE DATABASE IF NOT EXISTS k8s_community;
 CREATE DATABASE IF NOT EXISTS github_integration;
