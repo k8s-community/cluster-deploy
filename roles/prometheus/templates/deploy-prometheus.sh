c13de9bb25431700ccea01c0f2a1c4aadb0beecf
vsaveliev <savelievharlam@gmail.com>
2017-10-24 15:06:24 +0700

    updated prometheus to 2.0-rc1 + fixes for it

--- roles/prometheus/templates/deploy-prometheus.sh
+++ roles/prometheus/templates/deploy-prometheus.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function deploy_prometheus {
-   if kubectl get deploy -l k8s-app=prometheus | grep prometheus &> /dev/null; then
-       echo "Prometheus already exists"
+   if kubectl get deploy -l app=prometheus -n {{ k8s_prometheus_namespace }} | grep prometheus &> /dev/null; then
+       echo "Updating Prometheus (config-maps will be auto applied)"
+       kubectl apply -f {{ k8s_addons_dir }}/prometheus.yaml
    else
        echo "Creating Prometheus"
        kubectl create -f {{ k8s_addons_dir }}/prometheus.yaml
